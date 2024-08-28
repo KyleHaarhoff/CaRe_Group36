5f761efe724db63ff71d5882b2f6f6c428b318a3
Kyle Haarhoff <100769371+KyleHaarhoff@users.noreply.github.com>
2024-08-28 18:16:22 +0930

    Navbar initial Styling

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <header>
