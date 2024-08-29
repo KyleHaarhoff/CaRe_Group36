4c32cf1b43270324f00bfedf96a00e7847007532
Kyle Haarhoff <100769371+KyleHaarhoff@users.noreply.github.com>
2024-08-29 11:06:44 +0930

    fixed navbar css route

--- common/navbar/navbar.php
+++ common/navbar/navbar.php
@@ -2,7 +2,7 @@
     include_once __DIR__."/../../conf.php"
 ?>
 
-<link rel="stylesheet" type="text/css" href="common/navbar/navbar.css">
+<link rel="stylesheet" type="text/css" href="<?= $base_url ?>/common/navbar/navbar.css">
 <nav class="MainNavbar">
     <a href= "<?= $base_url ?>patient/home/home.php">Home</a><a>Journal</a><a>History</a>
 </nav>
