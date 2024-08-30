ecf8ac7c52b320b27e93ea5c30378258714aed10
Kyle Haarhoff <100769371+KyleHaarhoff@users.noreply.github.com>
2024-08-30 21:02:00 +0930

    fixed bug with css applying correctly to the navbar

---
+++ therapist/groups/groups.php
@@ -0,0 +1,22 @@
+<?php
+
+    include_once __DIR__."/../../conf.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Groups</title>
+    <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/therapist/groups/groups.css">
+</head>
+<body>
+    <header>
+        <?php include "../../common/navbar/navbar.php"; ?> 
+    </header>
+    <body>
+
+    </body>
+</body>
+</html>
