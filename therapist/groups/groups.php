6d7d7dc94e56413a3091fcf5208af1184c1f9b72
Kyle Haarhoff <100769371+KyleHaarhoff@users.noreply.github.com>
2024-08-30 22:30:27 +0930

    initial groups page

--- therapist/groups/groups.php
+++ therapist/groups/groups.php
@@ -16,7 +16,77 @@
         <?php include "../../common/navbar/navbar.php"; ?> 
     </header>
     <body>
-
+        <div class="groupComponentContainer shadow rounded">
+            <div class="listControlContainer">
+                <button class="careButton">Add Group</button>
+                <span class="listControls">
+                    <span class="groupFilterContainer">Filter</span>
+                    <input type="text" placeholder="Search.." class="careSearch">
+                </span>
+            </div>
+            <div class="groupListContainer">
+                <div class="groupContainer" data-groupID="">
+                    <div class="groupControl">
+                        <span class="groupTitle">Group 1</span>
+                        <span>
+                            <img class="dropdownIcon vInvert" src="<?= $base_url ?>assets/images/dropdown.svg">
+                        </span>
+                    </div>
+                    <div class="patientList">
+                        <span>Jack Ross</span>  
+                        <span>Ross Man</span>  
+                        <span>Frederick</span>  
+                        <span>Somebody</span>  
+                        <span>Another Body</span>  
+                        <span>Jack Ross Again</span>  
+                    </div>
+                    <div class="groupButtons">
+                        <button class="careButton">Manage Group</button>
+                        <button class="careButton">Delete Group</button>
+                    </div>
+                </div>
+                <div class="groupContainer" data-groupID="">
+                    <div class="groupControl">
+                        <span class="groupTitle">Group 2</span>
+                        <span>
+                            <img class="dropdownIcon vInvert" src="<?= $base_url ?>assets/images/dropdown.svg">
+                        </span>
+                    </div>
+                    <div class="patientList">
+                        <span>Jack Ross</span>  
+                        <span>Ross Man</span>  
+                        <span>Frederick</span>  
+                        <span>Somebody</span>  
+                        <span>Another Body</span>  
+                        <span>Jack Ross Again</span>  
+                    </div>
+                    <div class="groupButtons">
+                        <button class="careButton">Manage Group</button>
+                        <button class="careButton">Delete Group</button>
+                    </div>
+                </div>
+                <div class="groupContainer" data-groupID="">
+                    <div class="groupControl">
+                        <span class="groupTitle">Group 3</span>
+                        <span>
+                            <img class="dropdownIcon vInvert" src="<?= $base_url ?>assets/images/dropdown.svg">
+                        </span>
+                    </div>
+                    <div class="patientList">
+                        <span>Jack Ross</span>  
+                        <span>Ross Man</span>  
+                        <span>Frederick</span>  
+                        <span>Somebody</span>  
+                        <span>Another Body</span>  
+                        <span>Jack Ross Again</span>  
+                    </div>
+                    <div class="groupButtons">
+                        <button class="careButton">Manage Group</button>
+                        <button class="careButton">Delete Group</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </body>
 </html>
