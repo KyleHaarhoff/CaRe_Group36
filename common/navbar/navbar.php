7009558456b42f5f887cf2bc86766e45a01a7e7f
Kyle Haarhoff <100769371+KyleHaarhoff@users.noreply.github.com>
2024-08-30 16:43:40 +0930

    initial Login page, simple global styling, more navbar updates

--- common/navbar/navbar.php
+++ common/navbar/navbar.php
@@ -1,8 +1,30 @@
 <?php
     include_once __DIR__."/../../conf.php"
 ?>
-
+<link rel="stylesheet" type="text/css" href="<?= $base_url ?>/style.css">
 <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/common/navbar/navbar.css">
 <nav class="MainNavbar">
-    <a href= "<?= $base_url ?>patient/home/home.php">Home</a><a>Journal</a><a>History</a>
+
+    <?php 
+        //cuurently displaying content based on url
+        //will change to access control in further phases
+
+        if (str_contains($_SERVER['REQUEST_URI'], "patient"))
+        {
+        ?>
+            <a href= "<?= $base_url ?>patient/home/home.php"><p>Home</p></a>
+            <a><p>Journal</p></a>
+            <a><p>History</p></a>
+
+        <?php 
+        }
+        if (str_contains($_SERVER['REQUEST_URI'], "therapist"))
+        {
+        ?>
+            <a href= "<?= $base_url ?>patient/therapist/home.php"><p>Home</p></a>
+            <a><p>Journal</p></a>
+            <a><p>History</p></a>
+        <?php
+        }
+    ?>
 </nav>
