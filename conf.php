b1dad7f9a06b2bbd5cdaf2b0930fb587da76191e
beingdev1 <43754671+beingdev1@users.noreply.github.com>
2024-10-02 00:17:41 +0930

    Revert "Groups table db"

--- conf.php
+++ conf.php
@@ -1,8 +1,4 @@
 <?php
 $base_url = "/www/CaRe_Group36/";
 $patient_view_page = "/www/CaRe_Group36/therapist/patient_view_page/patient_view_page.php";
-
-// Include the database connection
-include_once __DIR__ . '/common/db/db-conn.php';
-
 ?>
